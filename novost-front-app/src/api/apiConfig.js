c62457554210e9a4962d79fd6ed3e9a1900a2935
JuanMartinezB <juans.martinezb@uqvirtual.edu.co>
2026-03-07 01:50:31 -0500

    Actualizado el Axios para contectar con el Back Desplegado

--- novost-front-app/src/api/apiConfig.js
+++ novost-front-app/src/api/apiConfig.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'http://18.190.234.243',
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://18.190.234.243',
 });
 
 api.interceptors.request.use(
