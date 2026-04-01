af68ea32ff7098f09af29263a105e369b7ce340a
David <bedoyaj919@gmail.com>
2026-04-01 12:39:47 -0500

    apiConfig despliegue y gitignore variables

--- novost-front-app/src/api/apiConfig.js
+++ novost-front-app/src/api/apiConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'http://18.190.234.243'
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://18.190.234.243'
+  //baseURL: 'http://localhost:8080',
 });
 
 api.interceptors.request.use(
